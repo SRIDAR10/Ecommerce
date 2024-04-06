ca97d2f127e5d90a94d31f9fb847bb9134b614a4
Sridar Saikrishnan <sridar.krishnan@preskale.com>
2024-04-06 11:03:44 +0530

    home page style alterations

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Carousel } from "antd";
+import { Carousel, Flex, Typography } from "antd";
+
+const { Title,Text } = Typography;
 
 const contentStyle = {
   marginTop: "20px",
@@ -9,57 +11,17 @@ const contentStyle = {
   lineHeight: "160px",
   textAlign: "center",
   background: "white",
-  paddingBottom:"1em"
+  paddingBottom: "1em",
 };
 
 const Home = () => {
   return (
-    <div>
-      <Carousel autoplay>
-        <div>
-          <h3 style={contentStyle}>
-            <img
-              className="caro-img"
-              src="https://5.imimg.com/data5/IG/NQ/MY-29577122/carbonfibertubes1_1533539506-1000x1000.jpg"
-              alt="Supreme CPVC Pipe"
-            />
-          </h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>
-          <img
-              className="caro-img"
-              src="https://5.imimg.com/data5/SELLER/Default/2023/7/326715895/GG/MT/KK/97135163/ppr-c-pipe-250x250.jpg"
-             
-              alt="Supreme CPVC Pipe"
-            />
-            
-               </h3>
-        </div>
-     
-        <div>
-          <h3 style={contentStyle}>
-          <img
-              className="caro-img"
-              src="https://5.imimg.com/data5/SELLER/Default/2023/6/319782870/WC/LA/WG/190423826/supreme-pvc-casing-pipe-2--500x500.jpg"
-             
-              alt="Supreme CPVC Pipe"
-            />
-          </h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>
-          <img
-              className="caro-img"
-              src="https://5.imimg.com/data5/SELLER/Default/2022/9/PB/KN/AA/6095921/prince-upvc-pipes-500x500.jpg"
-             
-              alt="Supreme CPVC Pipe"
-            />
-          </h3>
-        </div>
-      </Carousel>
-      <h1 style={{textAlign:"center"}}>About Us</h1>
-      <p className="about-txt">
+    <div className="home-page">
+      <img src="https://unitechpipes.com/wp-content/uploads/2020/09/6.jpg?id=4069" />
+      <div  className="padding-3em">
+      <Title style={{ marginTop: "20px" }} rootClassName="title" underline>About Our Company</Title>
+      <Flex vertical>
+      <Text className="text">
         Ns Polymers Private Limited is an unlisted private company incorporated
         on 19 March, 2023. It is classified as a private limited company and is
         located in , Tamil Nadu. It's authorized share capital is INR 15.00 lac
@@ -72,8 +34,62 @@ const Home = () => {
         Polymers Private Limited is U22191TZ2023PTC027938. The registered office
         of Ns Polymers Private Limited is at SELVARANI CAMPUS, ALAKKUVARPATTI
         VILLAGE, CHETTINAYAKKAN PATTI POSTCHETTINAYAKKAN PATTI - KALLIPATTI ROAD
-        Dindigul , Tamil Nadu.
-      </p>
+        Dindigul , Tamil Nadu.</Text>
+        <Text className="text mt-10">
+         Water is a necessity for every living organism to
+        survive and flourish. Water being the most precious resource its
+        excessive use has led to its dire scarcity too. We at UNITECH PVC Pipes
+        have been at the forefront of researching and developing various piping
+        systems for diverse applications. The vast country that India is
+        geographically, and demographically with a massive population it becomes
+        a mammoth task to provide access to safe drinking water. The frequency
+        of drought spells has increased leading to water scarcity which has led
+        to a huge demand for water and piping systems for conveyance and
+        management. We take pride in solving the water problem being renowned
+        and considered as the best manufacturer of PVC pipes in India for the
+        most effective piping systems for varied applications. Our company’s
+        objective is to meet the ever-growing demand of our clientele for waste
+        management, water supplies to industrial, residential, and agricultural
+        segments by developing and delivering a high-quality range of piping
+        systems. We understand the needs of our customers like no one else, and
+        this has made us strive for excellence in every project we undertake,
+        getting us recognition in India as well as around the world. All our
+        efforts have made Ns Polymers pipes win a great many awards and
+        accolades. Ns Polymers pipes are your one-stop shop for all piping
+        products. Our diverse and huge range of pipes and fittings are
+        manufactured according to International standards. We have always
+        endeavored to comply with the highest quality standards in the industry.
+        Our constant urge to innovate and create cost-effective solutions that
+        focus on finding a solution to the core problem of water and waste
+        management has made us the top PVC pipes company in India. Ns Polymers
+        pipes have a factory unit with huge infrastructure in Barnala with a
+        state of an art manufacturing entity. We are a proud team of engineers,
+        technical stewards, and sheer determined employees who endeavor to make
+        our company rank amongst the top 10 PVC companies in India.
+        </Text>
+        </Flex>
+        </div>
+      <Flex  align="center" justify="center" gap={20} className="padding-3em">
+        <img src="https://unitechpipes.com/wp-content/uploads/2020/10/1488-2048x1365.jpg" width={600} height={600}/>
+        <Flex vertical>
+          <Title rootClassName="title" underline>Our Mission</Title>
+          <Text className="text">
+            Maximize value in terms of client satisfaction by creating a high
+            performance organization with innovation and leadership. To strive
+            for excellence by maintaining and upgrading the standards in Quality
+            and Safety endeavors.
+          </Text>
+        </Flex>
+      </Flex>
+      <Flex align="center" justify="center" gap={20} className="padding-3em">
+      <Flex vertical>
+          <Title className="title" underline>Our Vision</Title>
+          <Text rootClassName="text">
+          We aim to be sincerely global and be the trendsetters in the industry by setting high values and morals. We see ourselves and our fellow men leading the path of success. We endeavor to create an open environment which promotes progressive learning and innovation.
+          </Text>
+        </Flex>
+        <img src="https://unitechpipes.com/wp-content/uploads/2020/10/1586-2048x1367.jpg" width={600} height={600}/>
+      </Flex>
     </div>
   );
 };
